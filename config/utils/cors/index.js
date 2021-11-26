6e2e2ca01e153f4d8afc7068410c33d2545d9dc7
Glipper 000 <husdadymena@gmail.com>
2021-11-26 02:43:18 -0500

    [updated] tables design

--- config/utils/cors/index.js
+++ config/utils/cors/index.js
@@ -4,13 +4,13 @@ const cors = require('cors')
 
 module.exports = function(app) {
   const corsOptions = {
-    origin: process.env.ORIGIN,
+    // origin: process.env.ORIGIN,
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: [
       'Origin',
       'Content-Type',
-      'X-Requested-With',
       'Accept',
       'Authorization',
       'SECRET_PASSWORD'
