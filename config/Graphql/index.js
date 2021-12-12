67223939cc7de223085fe5aa48fb3b2783c908e8
Glipper 000 <husdadymena@gmail.com>
2021-12-11 22:28:23 -0500

    []

--- config/Graphql/index.js
+++ config/Graphql/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const GraphQLSchema = require('./schema')
-const { graphqlHTTP } = require('express-graphql')
-const { verifyToken } = require('@middlewares/auth/token')
+// const GraphQLSchema = require('./schema')
+// const { graphqlHTTP } = require('express-graphql')
+// const { verifyToken } = require('@middlewares/auth/token')
 
 module.exports = function(app) {
   app.post(
