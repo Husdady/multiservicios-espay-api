524a9dd1a42c2d72cdb830bf96364ccc9c8fffbf
Glipper 000 <husdadymena@gmail.com>
2021-12-11 23:05:44 -0500

    []

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const app = express()
 
+require('./config/graphql')
+
 const port = process.env.PORT
 app.listen(port || 4000, () => {
   console.log('[INFO]', `The server is running on ${process.env.PUBLIC_URL}`)
