b8f89ef57e6592c052f2678a40b1c81c0badf82e
Glipper 000 <husdadymena@gmail.com>
2021-12-11 23:20:45 -0500

    []

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 require('module-alias/register')
-require('@graphql/index')
+require('@graphql/queries')
 
 const port = process.env.PORT
 app.listen(port || 4000, () => {
